55a29755f6cb337222dc9390c5184f0153aca43e
Oleksii SHCHELKONOHOV <jobmanalex@gmail.com>
2025-09-06 13:55:02 +0300

    camera inversion fix

--- input_handler.py
+++ input_handler.py
@@ -1,5 +1,6 @@
 import pygame
 from pygame.locals import *
+import math
 
 class InputHandler:
     def __init__(self, camera, renderer):
@@ -14,15 +15,17 @@ class InputHandler:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 return False # Signal to quit
 
-            self.handle_keyboard_input(pygame.key.get_pressed())
             self.handle_mouse_input(event)
+        
+        self.handle_keyboard_input(pygame.key.get_pressed())
         return True # Signal to continue
 
     def handle_keyboard_input(self, keys):
+        inversion_factor = -1 if math.cos(self.camera.angle_x) < 0 else 1
         if keys[pygame.K_w]: self.camera.angle_x_vel += self.camera.keyboard_rotation_speed
         if keys[pygame.K_s]: self.camera.angle_x_vel -= self.camera.keyboard_rotation_speed
-        if keys[pygame.K_a]: self.camera.angle_y_vel += self.camera.keyboard_rotation_speed
-        if keys[pygame.K_d]: self.camera.angle_y_vel -= self.camera.keyboard_rotation_speed
+        if keys[pygame.K_a]: self.camera.angle_y_vel += self.camera.keyboard_rotation_speed * inversion_factor
+        if keys[pygame.K_d]: self.camera.angle_y_vel -= self.camera.keyboard_rotation_speed * inversion_factor
 
     def handle_mouse_input(self, event):
         if event.type == pygame.KEYDOWN:
@@ -40,6 +43,7 @@ class InputHandler:
                 self.mouse_dragging = False
         elif event.type == pygame.MOUSEMOTION:
             if self.mouse_dragging:
+                inversion_factor = -1 if math.cos(self.camera.angle_x) < 0 else 1
                 rel_x, rel_y = event.rel
-                self.camera.angle_y_vel += rel_x * self.camera.rotation_sensitivity * 0.01
+                self.camera.angle_y_vel += rel_x * self.camera.rotation_sensitivity * 0.01 * inversion_factor
                 self.camera.angle_x_vel += rel_y * self.camera.rotation_sensitivity * 0.01
